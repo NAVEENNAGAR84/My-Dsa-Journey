fa47662c49f0a4b96f0622feb72ea36ce82b7547
NAVEENNAGAR84 <nagarnaveen948@gmail.com>
2026-06-21 11:28:07 +0530

    Day:17

---
+++ validperfectsquare.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num==0 || num==1)
+            return true;
+        long long start=1;
+        long long end= num/2;
+        while(start<=end)
+        {
+            long long mid= start+(end-start)/2;
+             if(mid*mid ==num)
+             {
+              return true;
+                
+             }
+             else if (mid*mid<num)
+             { 
+                 start=mid+1;
+ 
+               
+             }
+             else
+             {
+                end= mid-1;
+
+             }
+        }
+        return false;
+    
+       
+       
+    }
+};
