9e8da7d337f01ae26eb618434fbacf24d1ae9a24
NAVEENNAGAR84 <nagarnaveen948@gmail.com>
2026-08-31 18:29:28 +0530

    Time: 0 ms (100%), Space: 8 MB (86.13%) - LeetHub

---
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        
+        for(int w=sqrt(area);w>=0;w--)
+        {
+            if(area%w==0)
+            {
+                int l=area/w;
+                return {l,w};
+                
+            }
+        }
+        return {};
+        
+    }
+};
